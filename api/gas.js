eb87628271167295556421bef3041b06d8e5b7b0
danilo613 <danilo@starseeds.me>
2026-03-21 11:50:56 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,14 +1,19 @@
-
 export default async function handler(req, res) {
   const GAS_URL = "https://script.google.com/macros/s/AKfycbxv_el3FDOfKuMutYwZTd2NGY3uyLZzGgyYzeuF18iyhqi3P3azdEDzxPhjueFjuepXcQ/exec";
   const query = new URLSearchParams(req.query).toString();
   const url = query ? `${GAS_URL}?${query}` : GAS_URL;
   
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await fetch(url, {
+      redirect: "follow",
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    });
+    const text = await response.text();
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json(data);
+    res.setHeader("Content-Type", "application/json");
+    res.send(text);
   } catch(e) {
     res.status(500).json({ error: e.message });
   }
