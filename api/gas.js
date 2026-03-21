e65898f53ff3dcd11fd505cba0f45b17ad4734db
danilo613 <danilo@starseeds.me>
2026-03-21 12:10:28 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,31 +1,18 @@
-const https = require("https");
-const http = require("http");
+export const config = { runtime: "edge" };
 
-function fetchUrl(url) {
-  return new Promise((resolve, reject) => {
-    const client = url.startsWith("https") ? https : http;
-    client.get(url, (res) => {
-      if (res.statusCode === 301 || res.statusCode === 302 || res.statusCode === 307) {
-        return fetchUrl(res.headers.location).then(resolve).catch(reject);
-      }
-      let data = "";
-      res.on("data", chunk => data += chunk);
-      res.on("end", () => resolve(data));
-    }).on("error", reject);
+export default async function handler(req) {
+  const GAS = "https://script.google.com/macros/s/AKfycbxc5jypY5N-Z2Zsj2qtWQKVaRg76GJfzLa1UX1dPlRZfNKJhJFh1vbglT_iw1QLs49OJA/exec";
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.toString();
+  const url = query ? `${GAS}?${query}` : GAS;
+
+  const res = await fetch(url);
+  const text = await res.text();
+
+  return new Response(text, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
-
-module.exports = async function handler(req, res) {
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbxv_el3FDOfKuMutYwZTd2NGY3uyLZzGgyYzeuF18iyhqi3P3azdEDzxPhjueFjuepXcQ/exec";
-  const query = new URLSearchParams(req.query).toString();
-  const url = query ? `${GAS_URL}?${query}` : GAS_URL;
-  
-  try {
-    const text = await fetchUrl(url);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json");
-    res.send(text);
-  } catch(e) {
-    res.status(500).json({ error: e.message });
-  }
-};
