7b325b42a73133eee6e4424c3483489bac5fdc5f
danilo613 <danilo@starseeds.me>
2026-03-22 14:48:42 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,4 +1,5 @@
 export const config = { runtime: "edge" };
+
 export default async function handler(req) {
   const GAS = "https://script.google.com/macros/s/AKfycbyxylgMmIFylS3hzkwZWlFnZzi0lskEsq7j2cwdePaThHWLNPC6GqxlJZ7zT-g0Jbn5lg/exec";
   const { searchParams } = new URL(req.url);
@@ -7,6 +8,9 @@ export default async function handler(req) {
   const res = await fetch(url);
   const text = await res.text();
   return new Response(text, {
-    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
