0b75b7cae54ef99f401f04159ea57c70f31e015a
danilo613 <danilo@starseeds.me>
2026-03-22 14:38:02 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,6 +1,17 @@
 export const config = { runtime: "edge" };
 export default async function handler(req) {
-  const GAS = "https://script.google.com/macros/s/AKfycbzEDD1I5RCnQdQ-7qTvRv__BNpOF06BOaUO2q6MwOS0MZjFrN5P-bn4puRP2S5dJtXqQw/exec";
+  const GAS = "https://script.google.com/macros/s/AKfycbyxylgMmIFylS3hzkwZWlFnZzi0lskEsq7j2cwdePaThHWLNPC6GqxlJZ7zT-g0Jbn5lg/exec";
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.toString();
+  const url = query ? `${GAS}?${query}` : GAS;
+  const res = await fetch(url);
+  const text = await res.text();
+  return new Response(text, {
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  });
+}export const config = { runtime: "edge" };
+export default async function handler(req) {
+  const GAS = "https://script.google.com/macros/s/AKfycbyxylgMmIFylS3hzkwZWlFnZzi0lskEsq7j2cwdePaThHWLNPC6GqxlJZ7zT-g0Jbn5lg/exec";
   const { searchParams } = new URL(req.url);
   const query = searchParams.toString();
   const url = query ? `${GAS}?${query}` : GAS;
