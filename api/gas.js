7ddf2c247b271ae40a25226c2c8f8812e590dc15
danilo613 <danilo@starseeds.me>
2026-03-26 10:53:11 +0900

    Update gas.js

--- api/gas.js
+++ api/gas.js
@@ -1,6 +1,6 @@
 export const config = { runtime: "edge" };
 export default async function handler(req) {
-  const GAS = "https://script.google.com/macros/s/AKfycbw-GZNu2iE9QIdJ2R18fSa8xIpdV13W8yLE4g6qi0P442YQ8kYm-rtzAcbZr-7Tt8XoeA/exec";
+  const GAS = "https://script.google.com/macros/s/AKfycbyHN1asX0CeaeNwNkNrsnPIDOx4RlNnt64ZzmasO2qnUmyXCcCaWOHbHrdWMobsfIAqjg/exec";
   const { searchParams } = new URL(req.url);
   const query = searchParams.toString();
   const url = query ? `${GAS}?${query}` : GAS;
